0fdb4d14f0754a38c2efdfa205f93435d062b3d1
JacksonLiddell <157421584+JacksonLiddell@users.noreply.github.com>
2024-01-24 15:19:47 -0600

    Jan 24

---
+++ MyName.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_btn_show_pressed():
+	$Label.text="Jackson Liddell"
+
+
+func _on_btn_clear_pressed():
+	$Label.text=""
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()
